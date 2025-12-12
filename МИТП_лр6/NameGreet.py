e201dfc56b563b0accf463b010fda9d25d1a3ddd
ekaterina_zvereva <[ykabra123456@gmail.com]>
2025-12-12 23:22:03 +0300

    Добавлено приветствие

---
+++ МИТП_лр6/NameGreet.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    greeting = None
+    name = ''
+    
+    if request.method == 'POST':
+        name = request.form.get('name', '').strip()
+        
+        if name:
+            greeting = f"Hello, {name}! Nice to see you!"
+        else:
+            greeting = "Please, enter your name!"
+    
+    return render_template('index.html', 
+                         greeting=greeting, 
+                         name=name)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
